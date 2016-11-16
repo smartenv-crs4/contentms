14042c0c7ae94280be22a967c8b858d85f132e79
guido <guido.porruvecchio@gmail.com>
2016-11-16 10:51:57 +0100

    Miscellaneous fixes
    - Added some config properties to be passed also via CLI
    - Changed test environment default parameters

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -7,7 +7,7 @@ var auth = require('tokenmanager');
 var authField = config.decodedTokenFieldName;
 
 auth.configure({
-  authoritationMicroserviceUrl:config.authHost,
+  authoritationMicroserviceUrl:config.authProtocol + "://" + config.authHost + ":" + config.authPort,
   decodedTokenFieldName:config.decodedTokenFieldName,
   access_token:config.access_token});
 
