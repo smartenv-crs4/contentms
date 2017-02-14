ea29fcf9c8b8338843de1192e0ca70a96b682dba
Dino Soru <dino.soru@gmail.com>
2017-02-14 16:59:56 +0100

    json in response risultati

--- routes/contents/contents_search.js
+++ routes/contents/contents_search.js
@@ -39,7 +39,7 @@ module.exports = function(req, res, next) {
 
   content.findFiltered(filter, limit, skip)
   .then(result => {
-    res.send(result);
+    res.json(result);
   })
   .catch(e => { 
     console.log(e);
