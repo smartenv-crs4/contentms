00f9b081e631ff43a24db3be5b26dcb43bed2916
Dino Soru <dino.soru@gmail.com>
2017-04-21 17:43:07 +0200

    refuso cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,7 @@
 module.exports = exports = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With, x-dino');
+    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
     if ('OPTIONS' == req.method) res.sendStatus(200);
     else next();
 }
