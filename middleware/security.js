bd209a0a676665dc8610e196ed40621970874e27
dino <dino.soru@gmail.com>
2017-10-31 16:46:15 +0100

    fix new tokenmanager

--- middleware/security.js
+++ middleware/security.js
@@ -24,7 +24,7 @@ module.exports = {
   authWrap : (req, res, next) => {
     if(req.app.get("nocheck")) { //In dev mode non richiede il ms authms, usa utente fake passato da url TODO rimuovere?
         req[authField] = {};
-        req[authField]._id = req.query.fakeuid;
+        req[authField].token._id = req.query.fakeuid;
         next();
     }
     else auth.checkAuthorization(req, res, next);
