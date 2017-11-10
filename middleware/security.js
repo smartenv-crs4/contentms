370ec24287b0ba02e6c3b17426c24a8f52c7a91b
dino <dino.soru@gmail.com>
2017-11-10 11:16:12 +0100

    aggiunta auth su utente admin globale

--- middleware/security.js
+++ middleware/security.js
@@ -1,16 +1,25 @@
 const authField = require('propertiesmanager').conf.decodedTokenFieldName;
 const auth = require('tokenmanager');
+const rp = require('request-promise');
 var content = require('../schemas/content.js').content;
 
 module.exports = {
   isContentAdmin: (req, res, next) => {
     let uid = req[authField].token._id;
     let content_id = req.params.id;
+    let content_data = undefined;
     content.findById(content_id)
     .then(c => {
+      content_data = c;
+      return rp(config.authUrl + (config.authUrl.endsWIth('/') ? '' : '/') + 'tokenactions/getsupeusertokenlist')
+    })
+    .then(tokenarray => {
+      let isGlobalAdmin = tokenarray.indexOf(req[authField].token.type) != -1;
       if(!uid)
         res.boom.forbidden('User identifier missing')
-      else if((c.admins.indexOf(uid) != -1) || uid == c.owner)
+      else if((c.admins.indexOf(uid) != -1) 
+              || uid == c.owner
+              || isGlobalAdmin)
         next();
       else
         res.boom.forbidden('You are not administrator for this content');
