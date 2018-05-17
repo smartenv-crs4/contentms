38ef535f1f00d7c710ea8a6245de2fe1b0fff1bc
dino <dino.soru@gmail.com>
2018-05-17 15:06:04 +0200

    lock on delete content

--- routes/contents/contents_delete.js
+++ routes/contents/contents_delete.js
@@ -13,13 +13,22 @@ var content = require('../../schemas/content.js').content;
  * @apiUse ServerError
  */
 module.exports = function(req, res, next) {
-  let id = req.params.id
-  content.delete(id)
-  .then(del => {
-    res.json(del);
-  })
-  .catch(e => {
-    console.log(e);
-    res.status(e.status||500).send({error:e.error||"server error"});
-  }); 
+    let id = req.params.id
+    content.update(id, {published:false}) //lock contenuto per evitare 
+    .then(r => {
+        if(!r.published)
+            return content.delete(id)
+        else throw({status:0})
+    })
+    .then(del => {
+        res.json(del);
+    })
+    .catch(e => {
+        if(e.status == '0')
+            res.boom.badImplementation("Unable to lock content before deletion");
+        else {
+            console.log(e);
+            res.status(e.status||500).send({error:e.error||"server error"});
+        }
+    }); 
 }
